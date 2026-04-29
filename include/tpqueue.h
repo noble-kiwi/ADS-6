42e0f0619daccf8f16a07c715bb3249cae6a009b
noble_kiwi <iwkr1208@gmail.com>
2026-04-29 09:58:31 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,55 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    struct Node {
+        T item;
+        Node* nextNode;
+        Node(const T& element) : item(element), nextNode(nullptr) {}
+    };
+    Node* top;
+
+ public:
+    TPQueue() : top(nullptr) {}
+
+    ~TPQueue() {
+        while (top) {
+            Node* tempNode = top;
+            top = top->nextNode;
+            delete tempNode;
+        }
+    }
+
+    TPQueue(const TPQueue&) = delete;
+    TPQueue& operator=(const TPQueue&) = delete;
+
+    void push(const T& val) {
+        Node* newNode = new Node(val);
+        if (!top || top->item.priority < val.priority) {
+            newNode->nextNode = top;
+            top = newNode;
+        } else {
+            Node* current = top;
+            while (current->nextNode && current->nextNode->item.priority >= val.priority) {
+                current = current->nextNode;
+            }
+            newNode->nextNode = current->nextNode;
+            current->nextNode = newNode;
+        }
+    }
+
+    T pop() {
+        Node* tempNode = top;
+        T popped = tempNode->item;
+        top = top->nextNode;
+        delete tempNode;
+        return popped;
+    }
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char character;
+    int priority;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
